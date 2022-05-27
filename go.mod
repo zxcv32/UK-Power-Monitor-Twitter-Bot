e45b7ff8a308ddbfc207e6eb3ec7d4c619ddbd91
Ashwani Sharma <i14a23h19a@gmail.com>
2022-05-27 22:09:11 +0530

    wip: read/write/update

---
+++ go.mod
@@ -0,0 +1,23 @@
+module zxcv32/upmtb
+
+go 1.18
+
+require (
+	github.com/dghubble/go-twitter v0.0.0-20220428155120-ee736133298b
+	github.com/dghubble/oauth1 v0.7.1
+	github.com/influxdata/influxdb-client-go/v2 v2.9.0
+	github.com/joho/godotenv v1.4.0
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/deepmap/oapi-codegen v1.8.2 // indirect
+	github.com/dghubble/sling v1.4.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"time"
+)
+
+// TwitterCredentials for oauth1.0a authentication
+type TwitterCredentials struct {
+	ApiKey       string
+	ApiSecret    string
+	AccessToken  string
+	AccessSecret string
+}
+
+// InfluxDbConfig for reading InfluxDB data
+type InfluxDbConfig struct {
+	BaseUrl     string
+	BucketPower string
+	BucketTweet string
+	Org         string
+	Token       string
+}
+
+type TweetDbRecord struct {
+	Status       string
+	TweetContent string
+	TweetId      int64
+	Error        string
+}
+
+// buildTwitterClient builds a Twitter client and validate credentials
+func buildTwitterClient(creds *TwitterCredentials) (*twitter.Client, error) {
+
+	config := oauth1.NewConfig(creds.ApiKey, creds.ApiSecret)
+	token := oauth1.NewToken(creds.AccessToken, creds.AccessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Build Twitter client
+	client := twitter.NewClient(httpClient)
+
+	verifyParams := &twitter.AccountVerifyParams{
+		IncludeEntities: twitter.Bool(false),
+		SkipStatus:      twitter.Bool(true),
+		IncludeEmail:    twitter.Bool(true),
+	}
+
+	user, _, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		log.Debugln(err)
+		return nil, err
+	}
+	log.Debugf("Auth user: %s (%s)\n", user.Name, user.ScreenName)
+	return client, nil
+}
+
+func buildInfluxDbClient(config *InfluxDbConfig) (influxdb2.Client, error) {
+	client := influxdb2.NewClient(config.BaseUrl, config.Token)
+	pong, err := client.Ping(context.Background())
+	if !pong || err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func queryInfluxDb(config *InfluxDbConfig, query string) (*api.QueryTableResult, error) {
+	client, err := buildInfluxDbClient(config)
+	if err != nil {
+		log.Errorln(err)
+		return nil, err
+	}
+	// Get query client
+	queryAPI := client.QueryAPI(config.Org)
+	result, err := queryAPI.Query(context.Background(), query)
+	if err == nil {
+		if result.Err() != nil {
+			log.Errorf("Query error: %s", result.Err().Error())
+		}
+	} else {
+		log.Errorf(err.Error())
+		return nil, err
+	}
+	client.Close()
+	return result, nil
+}
+
+// tweet Tweet stat
+func tweet(client *twitter.Client, tweetContent string) (*twitter.Tweet, error) {
+	tweet, resp, err := client.Statuses.Update(tweetContent, nil)
+	if err != nil {
+		return nil, err
+		log.Debugln(err)
+	}
+	log.Debugf("Status: %s\n", resp.Status)
+	log.Debugln("Tweet ID: %d\n", tweet.ID)
+	return tweet, nil
+}
+
+// writeTweetToDb Write tweet data to InfluxDB
+func writeTweetToDb(config *InfluxDbConfig, data TweetDbRecord) {
+	client, err := buildInfluxDbClient(config)
+	if err != nil {
+		log.Errorln(err)
+		return
+	}
+	record := fmt.Sprintf(`%s,tweet=monitor id=%d,status="%s",content="%s",error="%s"`, config.BucketTweet, data.TweetId, data.Status, data.TweetContent, data.Error)
+	writeAPI := client.WriteAPI(config.Org, config.BucketTweet)
+	writeAPI.WriteRecord(record)
+	writeAPI.Flush()
+	defer client.Close()
+}
+
+func getLastPowerStatus(config *InfluxDbConfig) string {
+	response, err := queryInfluxDb(config, `
+			from(bucket: "`+config.BucketPower+`")
+			  |> range(start: -1s, stop: now())
+			  |> filter(fn: (r) => r["_measurement"] == "lab")
+			  |> filter(fn: (r) => r["_field"] == "status")
+			  |> yield(name: "last")
+		`)
+	if err != nil {
+		log.Errorln(err)
+	}
+	var result []string
+	for response.Next() {
+		values := response.Record().Values()
+		value, exists := values["_value"]
+		if exists {
+			result = append(result, fmt.Sprintf(fmt.Sprintf("%s", value)))
+		}
+	}
+	if len(result) < 1 {
+		return ""
+	}
+	return result[0]
+}
+
+func getLastTweetStatus(config *InfluxDbConfig) string {
+	response, err := queryInfluxDb(config, `
+			from(bucket: "`+config.BucketTweet+`")
+			  |> range(start: -1d)
+			  |> filter(fn: (r) => r["_measurement"] == "twitter")
+			  |> filter(fn: (r) => r["_field"] == "status")
+			  |> filter(fn: (r) => r["tweet"] == "monitor")
+		      |> last()
+		`)
+	if err != nil {
+		log.Errorln(err)
+	}
+	var result []string
+	for response.Next() {
+		values := response.Record().Values()
+		value, exists := values["_value"]
+		if exists {
+			result = append(result, fmt.Sprintf(fmt.Sprintf("%s", value)))
+		}
+	}
+	if len(result) < 1 {
+		return ""
+	}
+	return result[0]
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Errorln("No .env file found")
+	}
+	twitterCredentials := TwitterCredentials{
+		ApiKey:       os.Getenv("API_KEY"),
+		ApiSecret:    os.Getenv("API_SECRET"),
+		AccessToken:  os.Getenv("ACCESS_TOKEN"),
+		AccessSecret: os.Getenv("ACCESS_SECRET"),
+	}
+
+	client, err := buildTwitterClient(&twitterCredentials)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	influxDbConfig := InfluxDbConfig{
+		BaseUrl:     os.Getenv("INFLUXDB_BASE_URL"),
+		BucketPower: os.Getenv("INFLUXDB_BUCKET_POWER"),
+		BucketTweet: os.Getenv("INFLUXDB_BUCKET_TWEET"),
+		Org:         os.Getenv("INFLUXDB_ORG"),
+		Token:       os.Getenv("INFLUXDB_TOKEN"),
+	}
+
+	for range time.Tick(1 * time.Second) {
+		lastPowerStatus := getLastPowerStatus(&influxDbConfig)
+		lastTweetStatus := getLastTweetStatus(&influxDbConfig)
+		if lastTweetStatus != lastPowerStatus {
+			tweetContent := fmt.Sprintf("Power Status: %s\nDetection Timestamp: %s", lastPowerStatus, time.Now().Format(time.RFC1123))
+			tw, err := tweet(client, tweetContent)
+			errorString := ""
+			var twId int64 = -1
+			if err != nil {
+				errorString = err.Error()
+				log.Errorln()
+			} else {
+				twId = tw.ID
+			}
+			tweetDbRecord := TweetDbRecord{
+				Status:       lastPowerStatus,
+				TweetContent: tweetContent,
+				TweetId:      twId,
+				Error:        errorString,
+			}
+			writeTweetToDb(&influxDbConfig, tweetDbRecord)
+		}
+	}
+}
